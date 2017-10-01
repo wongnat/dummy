cd46ed208331d82c36d5d2ed4e2818d388bf6796
Nathan Wong <nathan_wong@live.com>
2017-10-01 01:04:43 -0700

    migrate to amoeba.yml

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var sleep = require('system-sleep');
 
 while (true) {
-    console.log("Hello from latest commit container!");
+    console.log("Hello from container with amoeba.yml!");
     sleep(5000);
 }
